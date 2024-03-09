6758125df31bff24e7ed8bb22a8508274c498584
Ilya <sunycatmsk@gmail.com>
2024-03-09 09:25:30 +0300

    Add files of consignment microservice.

---
+++ consignment-cli/cli.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	pb "consignment-cli/proto/consignment"
+	"encoding/json"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"os"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	// Contact the server and print out its response.
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+}
